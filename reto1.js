f2d8415e08cc9bc866863e10cb6bdcafda0f0719
oficina <cevm.12293@hotmail.com>
2021-10-13 11:42:39 -0500

    Se cumple reto taller 1 JS-Practico

--- reto1.js
+++ reto1.js
@@ -1,25 +1,27 @@
 console.log("RETO TALLER 1 - TRIANGULO ISOSCELES");
-
+// formula para calculo de triangulo isosceles
 //h = sqrt((a^2)-((b^2)/4))
 
 function calcular_altura_isosceles(){
     const ladoA=document.getElementById("lado1");
     const ladoB=document.getElementById("lado2");
     const ladoC=document.getElementById("lado3");
-    const valueA=ladoA.value;
-    const valueB=ladoB.value;
-    const valueC=ladoC.value;
+    const valueA=Number(ladoA.value);
+    const valueB=Number(ladoB.value);
+    const valueC=Number(ladoC.value);
 
-    if (valueA == valueB){
+    if ((valueA == valueB)&&(valueA>valueC)){
         //valueC es la base
         formula(valueA,valueC);
-    }if (valueA == valueC){
+        return;
+    }if ((valueA == valueC)&&(valueA>valueB)){
         //valueB es la base
         formula(valueA,valueB);
-
-    }if (valueC==valueB){
+        return;
+    }if ((valueC == valueB)&&(valueC>valueA)){
         //valueA es la base
         formula(valueC,valueA);
+        return;
 
     }else{
         alert("NO ES UN TRIANGULO ISOSCELES")
@@ -28,7 +30,6 @@ function calcular_altura_isosceles(){
 }
 
 function formula(side,base){
-    const h = Math.sqrt((side^2)-((base^2)/4))
-
+    const h = Math.sqrt((side**2)-((base**2)/4))
     alert("La altura es "+h)
 }
